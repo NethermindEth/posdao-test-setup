bcf3eca8c7b5caf63ad6f5b7b9089d93c7ff0023
LukaszRozmej <lukasz.rozmej@gmail.com>
2020-11-12 20:53:38 +0100

    Add sleep after changing rules file

--- test/00_tx_priority.js
+++ test/00_tx_priority.js
@@ -1884,6 +1884,7 @@ async function saveConfigFile(config) {
   for (let t = 0; t < attempts; t++) {
     try {
       fs.writeFileSync(configFilepath, JSON.stringify(config, null, 2));
+	  await sleep(1000);
       break;
     } catch (e) {
       if (e.code == 'EBUSY') {
